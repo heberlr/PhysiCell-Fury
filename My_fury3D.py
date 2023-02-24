5fd18d228632b1591322e564f58c9e56b2716227
Heber Rocha <heberonly@gmail.com>
2023-02-24 11:54:22 -0500

    Update My_fury3D.py

--- My_fury3D.py
+++ My_fury3D.py
@@ -1,12 +1,13 @@
-from fury3D import CreateScene, CreateSnapshots
-from pyMCDS import pyMCDS
+from fury3D import CreateScene,
+import pcDataLoader as pc
 from pathlib import Path
 import numpy as np
 import pandas as pd
 import sys
+import pickle
 
 def my_custom_coloring_function(df_cells):
-    Gene_RFP = np.array([df_cells['genes_x']])
+    Gene_RFP = np.array([df_cells['genes_0']])
     RFP = np.where(Gene_RFP == 1)
     GFP = np.where(Gene_RFP == 0)
     # Colors
@@ -24,7 +25,7 @@ def my_custom_coloring_function(df_cells):
 
 def coloring_function_KI67_stain(df_cells):
     Cell_phase = np.array([df_cells['current_phase']])
-    Gene_RFP = np.array([df_cells['genes_x']])
+    Gene_RFP = np.array([df_cells['genes_0']])
     Ki67p = np.where(Cell_phase == 2)
     Ki67n = np.where(Cell_phase != 2)
     RFP = np.where(Gene_RFP == 1)
@@ -56,8 +57,9 @@ def my_custom_header_function(mcds):
     time_days = curr_time//1440.0
     time_hours = (curr_time%1440.0)//60
     time_min = ((curr_time%1440.0)%60)
+    df_Cells = mcds.get_cell_df()
     # Selecting RFP+ and GFP+ cells
-    Gene_RFP = np.array([mcds.data['discrete_cells']['genes_x']])
+    Gene_RFP = np.array([df_Cells['genes_0']])
     RFP = np.where(Gene_RFP == 1)
     GFP = np.where(Gene_RFP == 0)
     Count_RFP = len(RFP[1])
@@ -65,19 +67,45 @@ def my_custom_header_function(mcds):
     title_text = "Current time: %02d days, %02d hours, and %0.2f minutes \n\n RFP+: %d agents and GFP+: %d agents"%(time_days,time_hours,time_min,Count_RFP,Count_GFP)
     return title_text
 
+def generate_data_from_simualtions(folder, fileName):
+    import pickle
+    dic_data = {}
+    mcdsts = pc.pyMCDSts(folder, graph=False)  # generate a mcds time series instance
+    ls_xml = mcdsts.get_xmlfile_list()
+    l_mcds = mcdsts.read_mcds() # load all snapshots
+    # Make snapshots
+    for i, mcds in enumerate(l_mcds):
+        dic_data['domain_range'] = mcds.get_xyz_range()
+        dic_data['dx'], dic_data['dy'], dic_data['dx'] = mcds.get_xyz_range()
+        dic_data['time'] = mcds.get_time()
+        dic_data['df_cells']  = mcds.get_cell_df()
+    with open(fileName, 'wb') as file:
+        pickle.dump(dic_data, file)
+
 if __name__ == '__main__':
     if (len(sys.argv) != 3 and len(sys.argv) != 2):
       print("Please provide\n 1 arg [folder]: to taking snapshots from the folder \n or provide 2 args [folder] [frame ID]: to interact with scene!")
       sys.exit(1)
     if (len(sys.argv) == 3):
-      # AddBox = {'xmin': 600, 'xmax': 800, 'ymin': -370, 'ymax': -170, 'zmin': 150, 'zmax': 350} # output00 (200 microns^3)
-      AddBox = {'xmin': -70, 'xmax': 280, 'ymin': 650, 'ymax': 1000, 'zmin': 320, 'zmax': 670} # output01 (350 microns^3)
-      # CreateScene(Path(sys.argv[1]),"output%08d.xml"%int(sys.argv[2]), coloring_function=my_custom_coloring_function, header_function=my_custom_header_function, AddBox = AddBox)
-      # CreateScene(Path(sys.argv[1]),"output%08d.xml"%int(sys.argv[2]), coloring_function=coloring_function_KI67_stain, header_function=my_custom_header_function, BoxCrop = AddBox, PlotNucleus=True)
-      # SNAPSHOTS
-      # CreateSnapshots(Path(sys.argv[1]),file="output%08d.xml"%int(sys.argv[2]), coloring_function=my_custom_coloring_function, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, AddBox = AddBox)
-      # CreateSnapshots(Path(sys.argv[1]),file="output%08d.xml"%int(sys.argv[2]), coloring_function=my_custom_coloring_function, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, BoxCrop = AddBox)
-      # CreateSnapshots(Path(sys.argv[1]),file="output%08d.xml"%int(sys.argv[2]), coloring_function=coloring_function_KI67_stain, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, AddBox = AddBox)
-      CreateSnapshots(Path(sys.argv[1]),file="output%08d.xml"%int(sys.argv[2]), coloring_function=coloring_function_KI67_stain, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, BoxCrop = AddBox)
+      # AddBox = {'xmin': -250, 'xmax': 250, 'ymin': -250, 'ymax': 250, 'zmin': 500, 'zmax': 1000} # output_H1 (500 microns^3)
+      # PlaneYZ1 = -97
+      # PlaneYZ2 = 147
+
+      AddBox = {'xmin': 500, 'xmax': 1000, 'ymin': 500, 'ymax': 1000, 'zmin': -960, 'zmax': -460} # output_H2 (500 microns^3)
+      PlaneYZ1 = 580
+      PlaneYZ2 = 870
+
+      folder = Path(sys.argv[1])
+      file = "/output%08d.xml"%int(sys.argv[2])
+      CreateScene(folder,file,pc.pyMCDS(str(folder)+file, graph=False), coloring_function=my_custom_coloring_function, header_function=my_custom_header_function, PlotNucleus=True)
+      # CreateScene(folder,file,pc.pyMCDS(str(folder)+file, graph=False), coloring_function=my_custom_coloring_function, header_function=my_custom_header_function, BoxCrop = AddBox,PlotNucleus=True)
+
+      # SNAPSHOTS PostHypoxia stain
+      # CreateSnapshots(folder, coloring_function=my_custom_coloring_function, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, AddBox = AddBox, file=file, add_name='_Box')
+      # CreateSnapshots(folder, coloring_function=my_custom_coloring_function, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, BoxCrop = AddBox, file=file, add_name='_BoxCrop')
+      # SNAPSHOTS Ki67 stain
+      # CreateSnapshots(folder, coloring_function=coloring_function_KI67_stain, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, AddBox = AddBox, file=file, add_name='_Box_Ki67')
+      # CreateSnapshots(folder, coloring_function=coloring_function_KI67_stain, header_function=my_custom_header_function,size_window=(5000,5000), PlotNucleus=True, BoxCrop = AddBox, file=file, add_name='_BoxCrop_Ki67', PlaneYZ_1=PlaneYZ1, PlaneYZ_2=PlaneYZ2)
     if (len(sys.argv) == 2):
-      CreateSnapshots(Path(sys.argv[1]), coloring_function=my_custom_coloring_function, header_function=my_custom_header_function, PlotNucleus=True)
+        generate_data_from_simualtions(Path(sys.argv[1]))
+      # CreateSnapshots(Path(sys.argv[1]), coloring_function=my_custom_coloring_function, header_function=my_custom_header_function, size_window=(5000,5000), PlotNucleus=True)
